71464e51c5118c015d8f6cf89da7ad085738959e
euiseon34 <134220124+euiseon34@users.noreply.github.com>
2025-05-27 16:53:03 +0900

    Add files via upload
    랭크컨트롤러

---
+++ backend/src/main/java/capstone/backend/user/controller/ExperienceController.java
@@ -0,0 +1,25 @@
+package capstone.backend.user.controller;
+
+import capstone.backend.user.service.ExperienceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/experience")
+public class ExperienceController {
+
+    private final ExperienceService experienceService;
+
+    @PostMapping("/reward")
+    public ResponseEntity<String> reward(
+            @RequestParam Long userId,
+            @RequestParam int amount) {
+        experienceService.rewardExperience(userId, amount);
+        return ResponseEntity.ok("경험치 추가 완료");
+    }
+}
